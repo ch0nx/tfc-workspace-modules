84e7c38b2602fb777506bbb2c353f78d766cc4ab
ch0nx <105817987+ch0nx@users.noreply.github.com>
2022-05-18 12:28:22 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,12 +15,11 @@ variable "subnet_address_prefixes" {
   type = list
 }
 
-module "networking" {
-  source  = "app.terraform.io/fartknocker/tfc-workspace-modules/azurerm"
-  version = "0.12.0"
-
-  name                    = var.name
-  location                = var.location
-  vnet_address_spacing    = var.vnet_address_spacing
-  subnet_address_prefixes = var.subnet_address_prefixes
+resource "azurerm_subnet" "module" {
+  version = ">=0.12.0"
+  name                 = "${local.module_name}-subnet${count.index}"
+  count                = length(var.subnet_address_prefixes)
+  resource_group_name  = azurerm_resource_group.module.name
+  virtual_network_name = azurerm_virtual_network.module.name
+  address_prefixes       = [var.subnet_address_prefixes[count.index]]
 }
